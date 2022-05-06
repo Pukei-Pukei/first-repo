e867d279edc0dc877281266b5ba8f5e60f11b4da
Pukei-Pukei <mmbs23@naver.com>
2022-05-06 13:34:20 +0900

    iteration example

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
 print("Hello World\nProgramed to work and not to feel~")
+for i in range(20+1):
+    if i%3 + i%5 == 0:
+        print("yeardream")
+    elif i%3 == 0:
+        print("year")
+    elif i%5 == 0:
+        print("dream")
+    else:
+        print(i)
