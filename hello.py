9b18304bfc7d1bfab303b46e1da025ea63c6f56c
Pukei-Pukei <mmbs23@naver.com>
2022-05-06 13:44:18 +0900

    refactor: minor changes

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 print("Hello World\nProgramed to work and not to feel~")
 for i in range(20+1):
-    if i%3 + i%5 == 0:
+    if i%15 == 0:
         print("yeardream")
     elif i%3 == 0:
         print("year")
